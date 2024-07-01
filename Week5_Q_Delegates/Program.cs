aaa23230dbf4efbaa7003ea0a3dec9d6ae229c93
ABT Aurelien <aurelien@abrassart.net>
2024-07-01 14:05:00 +0200

    Ajoutez des fichiers projet.

---
+++ Week5_Q_Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Week5_Q_Delegates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ShowMoney money = Euro;
+            Console.WriteLine(money(10));
+            money = Dollar;
+            Console.WriteLine(money(10));
+        }
+
+        public delegate string ShowMoney(int money);
+
+        public static string Euro(int money)
+        {
+            return $"{money} euros";
+        }
+
+        public static string Dollar(int money)
+        {
+            return $"{money} dollars";
+        }
+    }
+}
